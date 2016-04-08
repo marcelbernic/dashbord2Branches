4b2db8b473456167ef27cdbe273972fc127c42fe
marcelbernic <marcel.bernic.1@ulaval.ca>
2016-04-08 16:06:54 -0400

    Remove stuff

--- publish-pages.sh
+++
@@ -1,7 +0,0 @@
-git checkout gh-pages
-cp dist/* .
-rm -rf dist
-git add *
-git commit -am "Deploy pages"
-git push origin gh-pages
-git checkout master
--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import { render } from 'react-dom';
-import React from 'react';
-
-import Application from 'components/application';
-
-render(
-  <Application/>,
-  document.getElementById('glo2003-project-dashboard'));
